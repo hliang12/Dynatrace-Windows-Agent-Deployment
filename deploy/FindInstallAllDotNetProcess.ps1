7256d855d0950c9bb5d34d07b6b099625d12492c
hliang12 <hao-lin.liang@dynatrace.com>
2017-09-14 09:17:52 +0100

    Add files via upload

--- deploy/FindInstallAllDotNetProcess.ps1
+++ deploy/FindInstallAllDotNetProcess.ps1
@@ -72,7 +72,7 @@ for($i=0; $i -lt $appPoolList.length; $i++){
 	
 		$temp = $appPoolList[$i] -match 'APPPOOL "(.*)"' ### regex out the process name 
 		Write-Host "Setting up agent for " $matches[1] 
-		$agentName = $matches[1]+$SystemProfile
+		$agentName = $matches[1]+ "_" + $SystemProfile
 		
 	    .\InstallDotNetAgent.psi $DTHOME $agentName $CollectorIP -Use64Bit '[ "w3wp.exe -ap \"$matches[1]\"" ]'
 		
